406b0278d46a35fef9f5ef21ca806ce844b0a3e6
onurersel <onurersel@gmail.com>
2019-09-17 17:16:54 +0200

    scale line height with font size

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ const realWidth = height > width ? width : height;
 const ScalableText = ({style, children, onlyScaleUp, ...props}) => {
   const fontSize = flattenStyle(style).fontSize || 14
   let maxSize = fontSize * 1.5
-  // Default line height is 120% of the font size.
-  const lineHeight = flattenStyle(style).lineHeight || fontSize * 1.2
-  const scaledLineHeight = Math.round(lineHeight * realWidth / 320)
+  //let scaledLineHeight = Math.round(lineHeight * realWidth / 320)
   var scaledFontSize = Math.round(fontSize * realWidth / 320)
   if (scaledFontSize < fontSize) scaledFontSize = fontSize
   if (scaledFontSize > maxSize) scaledFontSize = maxSize
+  // Default line height is 120% of the font size.
+  const lineHeightProp = flattenStyle(style).lineHeight
+  const scaledLineHeight = !!lineHeightProp ? lineHeightProp : scaledFontSize * 1.2
   return (
     <Text style={[style, {fontSize: scaledFontSize, lineHeight: scaledLineHeight}]} {...props}>
       {children}
