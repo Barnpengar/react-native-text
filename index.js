8416461d86ed10156d6b9f2ed4bd6664c17dc7c0
onurersel <onurersel@gmail.com>
2019-09-17 17:24:14 +0200

    scale line height with screen ratio

--- index.js
+++ index.js
@@ -9,12 +9,13 @@ const ScalableText = ({style, children, onlyScaleUp, ...props}) => {
   const fontSize = flattenStyle(style).fontSize || 14
   let maxSize = fontSize * 1.5
   //let scaledLineHeight = Math.round(lineHeight * realWidth / 320)
-  var scaledFontSize = Math.round(fontSize * realWidth / 320)
+  const scaleRatio = realWidth / 320
+  var scaledFontSize = Math.round(fontSize * scaleRatio)
   if (scaledFontSize < fontSize) scaledFontSize = fontSize
   if (scaledFontSize > maxSize) scaledFontSize = maxSize
   // Default line height is 120% of the font size.
   const lineHeightProp = flattenStyle(style).lineHeight
-  const scaledLineHeight = !!lineHeightProp ? lineHeightProp : scaledFontSize * 1.2
+  const scaledLineHeight = !!lineHeightProp ? lineHeightProp * scaleRatio : scaledFontSize * 1.2
   return (
     <Text style={[style, {fontSize: scaledFontSize, lineHeight: scaledLineHeight}]} {...props}>
       {children}
