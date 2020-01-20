5a0ce56e4997843d0e5a74052d0155f331f25227
onurersel <onurersel@gmail.com>
2020-01-20 10:41:57 +0100

    added hoc

--- index.js
+++ index.js
@@ -5,23 +5,26 @@ const {width, height} = Dimensions.get('window');
 const flattenStyle = StyleSheet.flatten;
 const realWidth = height > width ? width : height;
 
-const ScalableText = ({style, children, onlyScaleUp, allowFontScaling, ...props}) => {
-  const fontSize = flattenStyle(style).fontSize || 14
-  let maxSize = fontSize * 1.5
-  //let scaledLineHeight = Math.round(lineHeight * realWidth / 320)
-  const scaleRatio = realWidth / 320
-  var scaledFontSize = Math.round(fontSize * scaleRatio)
-  if (scaledFontSize < fontSize) scaledFontSize = fontSize
-  if (scaledFontSize > maxSize) scaledFontSize = maxSize
-  // Default line height is 120% of the font size.
-  const lineHeightProp = flattenStyle(style).lineHeight
-  const scaledLineHeight = !!lineHeightProp ? lineHeightProp * scaleRatio : scaledFontSize + 5
-  return (
-    <Text style={[style, allowFontScaling ? {fontSize: scaledFontSize, lineHeight: scaledLineHeight} : null]} {...props}>
-      {children}
-    </Text>
-  );
-};
+
+const withScalableText = (Component) => {
+  return ({style, children, onlyScaleUp, allowFontScaling, ...props}) => {
+    const fontSize = flattenStyle(style).fontSize || 14
+    let maxSize = fontSize * 1.5
+    const scaleRatio = realWidth / 320
+    var scaledFontSize = Math.round(fontSize * scaleRatio)
+    if (scaledFontSize < fontSize) scaledFontSize = fontSize
+    if (scaledFontSize > maxSize) scaledFontSize = maxSize
+    const lineHeightProp = flattenStyle(style).lineHeight
+    const scaledLineHeight = !!lineHeightProp ? lineHeightProp * scaleRatio : scaledFontSize + 5
+    return (
+      <Component style={[style, {fontSize: scaledFontSize, lineHeight: scaledLineHeight}]} allowFontScaling={allowFontScaling} {...props}>
+        {children}
+      </Component>
+    );
+  }
+}
+
+const ScalableText = withScalableText(Text)
 
 ScalableText.propTypes = {
   style: Text.propTypes.style,
@@ -34,3 +37,4 @@ ScalableText.defaultProps = {
 };
 
 export default ScalableText;
+export {withScalableText}
